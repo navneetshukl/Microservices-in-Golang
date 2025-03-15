0c2c0135f5eaac67bbeb2e32dd7c166ce1dd3ae4
navneetshukl <navneetshukla824@gmail.com>
2025-03-15 19:27:13 +0530

    logger-service is working fine

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -10,11 +10,17 @@ import (
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayLoad  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayLoad struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -38,29 +44,62 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 
 	switch requestPayload.Action {
 	case "auth":
-		app.authenticate(w,requestPayload.Auth)
+		app.authenticate(w, requestPayload.Auth)
+
+	case "log":
+		app.logItem(w, requestPayload.Log)
 
 	default:
 		app.errorJSON(w, errors.New("unknown action"))
 	}
 }
 
-func(app *Config)authenticate(w http.ResponseWriter,a AuthPayload){
-
-	// create some json we will send to auth microservice
-	jsonData,_:=json.MarshalIndent(a,"","\t")
-
-	// call the service
-	request,err:=http.NewRequest("POST","http://authentication-service/authenticate",bytes.NewBuffer(jsonData))
-	if err!=nil{
-		app.errorJSON(w,err)
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayLoad) {
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+	logServiceURL := "http://logger-service/log"
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
 		return
 	}
 
-	client:=&http.Client{}
-	response,err:=client.Do(request)
-	if err!=nil{
-		app.errorJSON(w,err)
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, err)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "logged"
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
+
+func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
+
+	// create some json we will send to auth microservice
+	jsonData, _ := json.MarshalIndent(a, "", "\t")
+
+	// call the service
+	request, err := http.NewRequest("POST", "http://authentication-service/authenticate", bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
 		return
 	}
 
@@ -68,11 +107,11 @@ func(app *Config)authenticate(w http.ResponseWriter,a AuthPayload){
 
 	// make sure we get back the correct status code
 
-	if response.StatusCode==http.StatusUnauthorized{
-		app.errorJSON(w,errors.New("invalid credentials"))
+	if response.StatusCode == http.StatusUnauthorized {
+		app.errorJSON(w, errors.New("invalid credentials"))
 		return
-	} else if response.StatusCode!=http.StatusAccepted{
-		app.errorJSON(w,errors.New("error calling auth service"))
+	} else if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("error calling auth service"))
 		return
 	}
 
@@ -81,21 +120,21 @@ func(app *Config)authenticate(w http.ResponseWriter,a AuthPayload){
 
 	//decode the json from the auth service
 
-	err=json.NewDecoder(response.Body).Decode(&jsonFromService)
-	if err!=nil{
-		app.errorJSON(w,err)
+	err = json.NewDecoder(response.Body).Decode(&jsonFromService)
+	if err != nil {
+		app.errorJSON(w, err)
 		return
 	}
 
-	if jsonFromService.Error{
-		app.errorJSON(w,err,http.StatusUnauthorized)
+	if jsonFromService.Error {
+		app.errorJSON(w, err, http.StatusUnauthorized)
 		return
 	}
 
 	var payload jsonResponse
-	payload.Error=false
-	payload.Message="Authenticated!"
-	payload.Data=jsonFromService.Data
+	payload.Error = false
+	payload.Message = "Authenticated!"
+	payload.Data = jsonFromService.Data
 
-	app.writeJSON(w,http.StatusAccepted,payload)
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
